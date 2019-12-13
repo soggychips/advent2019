41c7518b19ba4a6bedba9dcf2758d33885b2c684
Jacob Cohen <jacob.cohen@nbcuni.com>
2019-12-13 15:34:17 -0800

    added requests collector for input

--- lib.py
+++ lib.py
@@ -5,6 +5,22 @@ def get_input(day):
         return data[str(day)]
 
 
+def get_input_via_requests(day):
+    import os
+    if str(day) in os.listdir("input"):
+        print("Data for day {} found locally.".format(day))
+        text = open("input/{}".format(day), 'r').read()
+    else:
+        print("Data for day {} not found locally. Downloading...".format(day))
+        import requests
+        from config import cookies
+        url = "https://adventofcode.com/2019/day/{}/input".format(day)
+        text = requests.get(url, cookies=cookies).text.strip()
+        with open("input/{}".format(day), 'w') as f:
+            f.write(text)
+    return text.split('\n')
+
+
 def memoize(f):
     d = {}
 
